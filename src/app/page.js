d1a6b167989ad58420736dcf8e67271bfb5000fb
Zakir0702 <officialzak0702@gmail.com>
2025-03-24 20:50:14 +0530

    faq inserted

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,5 @@
 import About from "@/components/About";
-import Faq from "@/components/Faq";
+import Faq from "@/components/Faq/Faq";
 import Footer from "@/components/Footer";
 import Hero from "@/components/Hero";
 import PastEvents from "@/components/PastEvents";
