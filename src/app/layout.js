8a1034d21bb96a9c868ceac1334f3c9a82a765ae
Ishanvi Chauhan <ishanvi.chauhan@gmail.com>
2025-03-28 02:08:24 +0530

    Updated

--- src/app/layout.js
+++ src/app/layout.js
@@ -23,7 +23,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang="en" className={`bg-black`}>
       <body
         className={`${geistSans.variable} ${geistMono.variable} overflow-x-hidden antialiased`}
       >
